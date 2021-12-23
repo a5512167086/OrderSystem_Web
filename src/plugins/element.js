138522f1d953e47e87901e35902da239a6e32e5b
Shin <bear55121677@gmail.com>
2021-12-23 19:24:03 +0800

    feat(reversion): initialize version

---
+++ src/plugins/element.js
@@ -0,0 +1,7 @@
+import Vue from "vue";
+import Element from "element-ui";
+import locale from "element-ui/lib/locale/lang/zh-TW";
+import "element-ui/lib/theme-chalk/index.css";
+import "element-ui/lib/theme-chalk/reset.css";
+
+Vue.use(Element, { locale });
